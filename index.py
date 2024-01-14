cbd58c48e7f67c2ac3732eb02877c356cbc1390b
Akmaljon571 <100577151+Akmaljon571@users.noreply.github.com>
2024-01-14 09:20:50 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
-from aiogram import executor
+def main():
+    from aiogram import executor
 
-from setting import dp
-import message.index
-
+    from setting import dp
+    import message.index
+    
+    if __name__ == "__main__":
+        print('Ishlaymizmi 🫡')
+        executor.start_polling(dp, skip_updates=False)
 if __name__ == "__main__":
-    print('Ishlaymizmi 🫡')
-    executor.start_polling(dp, skip_updates=False)
+    main()
